ecada440b33dd1e0ea1c8445ad99a3d5218be623
eukah <kaiquehenry04@gmail.com>
2023-09-21 11:07:42 -0300

    Adicionamos Sprites

--- JS/mover.js
+++ JS/mover.js
@@ -11,16 +11,16 @@ function updatePersonagemPosition() {
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
-      posicaoVertical -= 5;
+      posicaoVertical -= 50;
       break;
     case 'ArrowDown':
-      posicaoVertical += 5;
+      posicaoVertical += 50;
       break;
     case 'ArrowLeft':
-      posicaoHorizontal -= 5;
+      posicaoHorizontal -= 50;
       break;
     case 'ArrowRight':
-      posicaoHorizontal += 5;
+      posicaoHorizontal += 50;
       break;
   }
   updatePersonagemPosition();
