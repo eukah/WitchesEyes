8dcb6c4310513e9bd351d24c5f53228b51a37ff4
eukah <kaiquehenry04@gmail.com>
2023-09-22 11:02:12 -0300

    Criamos colisão no inimigo e adicionamos mais "sprites" dos inimigos. E adicinamos o arquivo script para funções gerais

---
+++ JS/colisao.js
@@ -0,0 +1,39 @@
+//COLISÃO ===== INICIO
+function checkCollision() {
+    const personagemRect = personagem.getBoundingClientRect();
+    const inimigoRect = inimigo.getBoundingClientRect();
+  
+    if (
+      personagemRect.left < inimigoRect.right &&
+      personagemRect.right > inimigoRect.left &&
+      personagemRect.top < inimigoRect.bottom &&
+      personagemRect.bottom > inimigoRect.top
+    ) {
+      personagem.remove();
+    }
+  }
+//COLISAO ===== FIM
+//COLISAO NO INIMIGO ===== INICIO
+  function checkTiroInimigo(){
+    const tiro = document.getElementById('tiro'); // Mova esta linha para dentro da função
+    if (!tiro) return; // Se o tiro não existir, retorne e não faça nada
+
+    const tiroRect = tiro.getBoundingClientRect();
+    const inimigoRect = inimigo.getBoundingClientRect();
+
+    if (
+        tiroRect.left < inimigoRect.right &&
+        tiroRect.right > inimigoRect.left &&
+        tiroRect.top < inimigoRect.bottom &&
+        tiroRect.bottom > inimigoRect.top
+    ) {
+        inimigo.remove();
+    }
+}
+//COLISAO NO INIMIGO ===== FIM
+function checarColisoes(){
+	checkCollision();//Colisao entre inimigo e personagem
+	checkTiroInimigo();
+}
+
+setInterval(checarColisoes,50);
--- JS/mover.js
+++ JS/mover.js
@@ -11,16 +11,16 @@ function updatePersonagemPosition() {
 document.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'ArrowUp':
-      posicaoVertical -= 50;
+      posicaoVertical -= 10;
       break;
     case 'ArrowDown':
-      posicaoVertical += 50;
+      posicaoVertical += 10;
       break;
     case 'ArrowLeft':
-      posicaoHorizontal -= 50;
+      posicaoHorizontal -= 10;
       break;
     case 'ArrowRight':
-      posicaoHorizontal += 50;
+      posicaoHorizontal += 10;
       break;
   }
   updatePersonagemPosition();
